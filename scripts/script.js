2ffd2e3831558c2cc496e8c1d8b6ee546437033d
Md. Mahbub hiron <mhalom19@gmail.com>
2024-12-09 04:32:04 +0600

    Smart ticketing sytem all functionality completed

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
-console.log('hello ticketing system');
+const btns = document.querySelectorAll('.seat-button');
+for(const btn of btns){
+    btn.addEventListener('click', function(){
+        setBackgroundColor(btn);
+        updateSelectedSeatInfo('total-seat-price','seat-info',btn);
+        updateSeatCount('seat-count');
+        updateTotalSeat('total-seat');
+        updateGrandTotal('total-seat-price','grand-total-price');
+        isFinishedButtonDisable();
+        applyCouponDisable();
+    })
+}
+const successfulMessageBox = document.getElementById('successful-message');
+function finishedAndShowDialogBox(){
+    successfulMessageBox.style.display = 'block';
+}
+document.getElementById('close-message').addEventListener('click', function(){
+    successfulMessageBox.style.display = 'none';
+})
+
+function applyCoupon(){
+    applyCouponById('coupon-code','total-seat-price');
+}
